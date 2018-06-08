6a4f43121b9c61206419221d4b32375a54e15c27
DanNissen <d.edwardnissen@gmail.com>
2018-06-07 21:12:58 -0400

    -Removed errors from html
    -removed example ID names from modals
    -gave all id's unique names

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,8 +2,8 @@
 
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
-const authEvents = require('./auth-events')
-const gameEvents = require('./game-events')
+const authEvents = require('./auth/events')
+const gameEvents = require('./game/events')
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
