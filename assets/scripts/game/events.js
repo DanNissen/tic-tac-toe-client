eb6ddb6536a24be0c0700906fa2b4984dfc840ab
DanNissen <d.edwardnissen@gmail.com>
2018-06-08 16:38:59 -0400

    -game makes sure cells cannot be overwritten
    -dom changes to fill in cell with x or o
    -todo: function that checks for wins

--- assets/scripts/game/events.js
+++ assets/scripts/game/events.js
@@ -1,10 +1,10 @@
 'use strict'
-// const store = require('../store')
+const store = require('../store')
 
 const getFormFields = require('../../../lib/get-form-fields')
 const gameApi = require('./api')
 const gameUi = require('./ui')
-
+const gameLogic = require('./game-logic')
 // gameboard is a 3x3 grid composed of 9 html divs
 // divs are numbered 1-9 and are always in same placeholder
 // gameboard can be represented by an array index 0-8
@@ -12,14 +12,26 @@ const gameBoard = ['', '', '', '', '', '', '', '', '']
 // if the board is clicked we want to push a value of x or o to the array
 // depending on who clicks determines which letter
 // create players
-const player = 'x'
+
+let player = 'x'
 
 const onClick = () => {
   const cellId = event.target.id
   console.log('player', player, 'clicked on cell', cellId)
-  // push value to array
-  gameBoard[(cellId - 1)] = player
-  console.log(gameBoard)
+  // check to see if array is empty
+  if (gameBoard[(cellId - 1)] === '') {
+    // push value to array
+    gameBoard[(cellId - 1)] = player
+    $('#' + cellId).text(player)
+    console.log(gameBoard)
+    if (player === 'x') {
+      player = 'o'
+    } else if (player === 'o') {
+      player = 'x'
+    }
+  } else {
+    console.log('sorry that is not allowed')
+  } console.log(gameBoard)
 }
 
 module.exports = {
