ee99ff1990b4565912efffd048644004e9a8d898
DanNissen <d.edwardnissen@gmail.com>
2018-06-09 18:16:16 -0400

    -Completed game logic to check for winner
    -events.js calls functions in game-logic.js to perform game actions
    -click happens, check if cell is empty, if it is place player letter
    -if not nothing happens
    -then check for win
    -then switch switchPlayer
    -add functionality to stop game if winner is declared start tying in ui

--- assets/scripts/game/events.js
+++ assets/scripts/game/events.js
@@ -5,33 +5,16 @@ const getFormFields = require('../../../lib/get-form-fields')
 const gameApi = require('./api')
 const gameUi = require('./ui')
 const gameLogic = require('./game-logic')
-// gameboard is a 3x3 grid composed of 9 html divs
-// divs are numbered 1-9 and are always in same placeholder
-// gameboard can be represented by an array index 0-8
-const gameBoard = ['', '', '', '', '', '', '', '', '']
-// if the board is clicked we want to push a value of x or o to the array
-// depending on who clicks determines which letter
-// create players
-
-let player = 'x'
 
 const onClick = () => {
   const cellId = event.target.id
-  console.log('player', player, 'clicked on cell', cellId)
-  // check to see if array is empty
-  if (gameBoard[(cellId - 1)] === '') {
-    // push value to array
-    gameBoard[(cellId - 1)] = player
-    $('#' + cellId).text(player)
-    console.log(gameBoard)
-    if (player === 'x') {
-      player = 'o'
-    } else if (player === 'o') {
-      player = 'x'
-    }
+  if (gameLogic.isEmpty(cellId)) {
+    gameLogic.markCell(cellId)
+    gameLogic.checkForWin()
+    gameLogic.switchPlayer()
   } else {
-    console.log('sorry that is not allowed')
-  } console.log(gameBoard)
+    gameLogic.moveNotAllowed()
+  }
 }
 
 module.exports = {
--- assets/scripts/game/game-logic.js
+++ assets/scripts/game/game-logic.js
@@ -1,15 +1,67 @@
-// 'use strict'
-// const gameEvents = require('./events')
-// const switchPlayer = (player) => {
-//   if (player === 'x') {
-//     gameEvents.player = 'o'
-//   } else if (player === 'o') {
-//     gameEvents.player = 'x'
-//   }
-// }
-// module.exports = {
-//   switchPlayer
-// }
-const checkForWin = (array) => {
-  if array[1,4,7]
+'use strict'
+const gameEvents = require('./events')
+
+// gameboard is a 3x3 grid composed of 9 html divs
+// divs are numbered 1-9 and are always in same placeholder
+// gameboard can be represented by an array index 0-8
+const gameBoard = ['', '', '', '', '', '', '', '', '']
+
+let player = 'x'
+
+// check to see if cell is empty
+const isEmpty = (cellId) => {
+  console.log('player', player, 'clicked on cell', cellId)
+  if (gameBoard[(cellId - 1)] === '') {
+    return true
+  } else {
+    return false
+  }
+}
+// push value to array & mark cell
+const markCell = (cellId) => {
+  gameBoard[(cellId - 1)] = player
+  $('#' + cellId).text(player)
+  console.log(gameBoard)
+}
+
+const switchPlayer = () => {
+  if (player === 'x') {
+    player = 'o'
+  } else if (player === 'o') {
+    player = 'x'
+  }
+}
+
+const checkForWin = () => {
+  if (gameBoard[0] === player && gameBoard[1] === player && gameBoard[2] === player) {
+    console.log(player, 'has won the game!!')
+  } else if (gameBoard[3] === player && gameBoard[4] === player && gameBoard[5] === player) {
+    console.log(player, 'has won the game!!')
+  } else if (gameBoard[6] === player && gameBoard[7] === player && gameBoard[8] === player) {
+    console.log(player, 'has won the game!!')
+  } else if (gameBoard[0] === player && gameBoard[3] === player && gameBoard[6] === player) {
+    console.log(player, 'has won the game!!')
+  } else if (gameBoard[1] === player && gameBoard[4] === player && gameBoard[7] === player) {
+    console.log(player, 'has won the game!!')
+  } else if (gameBoard[2] === player && gameBoard[5] === player && gameBoard[8] === player) {
+    console.log(player, 'has won the game!!')
+  } else if (gameBoard[0] === player && gameBoard[4] === player && gameBoard[8] === player) {
+    console.log(player, 'has won the game!!')
+  } else if (gameBoard[2] === player && gameBoard[4] === player && gameBoard[6] === player) {
+    console.log(player, 'has won the game!!')
+  }
+}
+
+const moveNotAllowed = () => {
+  console.log('sorry that is not allowed')
+}
+
+module.exports = {
+  switchPlayer,
+  gameBoard,
+  checkForWin,
+  player,
+  isEmpty,
+  markCell,
+  moveNotAllowed
 }
